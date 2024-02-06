78b8be832741a2d3f45e3114147177c8f96b4c91
root <root@uemlinux-test.csez.zohocorpin.com>
2024-02-06 13:56:57 +0530

    add

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -90,14 +90,9 @@ const postsSlice = createSlice({
                 state.error = action.error.message
             })
             .addCase(addNewPost.fulfilled, (state, action) => {
-                const sortedPosts = state.posts.sort((a, b) => {
-                    if (a.id > b.id) return 1
-                    if (a.id < b.id) return -1
-                    return 0
-                })
-
-                action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
-                action.payload.userId = Number(action.payload.userId)
+                // const sortedPosts = state.entities;
+                // action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
+                // action.payload.userId = Number(action.payload.userId)
                 action.payload.date = new Date().toISOString();
                 action.payload.reactions = {
                     thumbsUp: 0,
